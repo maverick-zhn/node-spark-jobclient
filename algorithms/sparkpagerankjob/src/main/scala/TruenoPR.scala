e81201d1582a19b768de3b239eb4bc7add4e5b42
Servio Palacios <servio@palacios.com>
2016-09-19 15:58:01 -0400

    [new algorithms]

--- algorithms/sparkpagerankjob/src/main/scala/TruenoPR.scala
+++ algorithms/sparkpagerankjob/src/main/scala/TruenoPR.scala
@@ -1,4 +1,145 @@
-/**
-  * Created by maverick on 9/19/16.
+/*
+
+ ________                                                 _______   _______
+/        |                                               /       \ /       \
+$$$$$$$$/______   __    __   ______   _______    ______  $$$$$$$  |$$$$$$$  |
+   $$ | /      \ / /|  / /| /      \ /       \  /      \ $$ |  $$ |$$ |__$$ |
+   $$ |/$$$$$$  |$$ |  $$ |/$$$$$$  |$$$$$$$  |/$$$$$$  |$$ |  $$ |$$    $$<
+   $$ |$$ |  $$/ $$ |  $$ |$$    $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$$$$$$  |
+   $$ |$$ |      $$ \__$$ |$$$$$$$$/ $$ |  $$ |$$ \__$$ |$$ |__$$ |$$ |__$$ |
+   $$ |$$ |      $$    $$/ $$       |$$ |  $$ |$$    $$/ $$    $$/ $$    $$/
+   $$/ $$/        $$$$$$/   $$$$$$$/ $$/   $$/  $$$$$$/  $$$$$$$/  $$$$$$$/
+
+ */
+
+/**      In God we trust
+  * Created by: Servio Palacios on 2016.09.19.
+  * Source: TruenoPR.scala
+  * Author: Servio Palacios
+  * Description: Spark Job Connector using REST API, using parameters
   */
+
+/**
+  * Run a dynamic version of PageRank returning a graph with vertex attributes containing the
+  * PageRank and edge attributes containing the normalized edge weight.
+  *
+  *  graph the graph on which to compute PageRank
+  *  tol the tolerance allowed at convergence (smaller => more accurate).
+  *  resetProb the random reset probability (alpha)
+  *
+  * @return the graph containing with each vertex containing the PageRank and each edge
+  *         containing the normalized weight.
+  */
+
+/* Package related to the Job Server */
+package spark.jobserver
+
+import com.typesafe.config.{Config, ConfigFactory}
+import scala.util.Try
+
+/* spark references */
+import org.apache.spark.{SparkConf, SparkContext}
+import com.datastax.spark.connector._
+
+/* GraphX references */
+import org.apache.spark.graphx._
+import org.apache.spark.graphx.VertexRDD
+import org.apache.spark.rdd.RDD
+
+
+object TruenoPR extends SparkJob {
+
+  def main(args: Array[String]) {
+
+    val conf = new SparkConf(true)
+      .set("spark.cassandra.connection.host", "localhost")
+      .setMaster("local[4]")
+      .setAppName("TruenoPR")
+
+    val sc = new SparkContext(conf)
+    val config = ConfigFactory.parseString("")
+    val results = runJob(sc, config)
+    println("Result is " + results)
+  }
+
+  /* Validate incoming parameters */
+  /* In here I use schemas to determine in which Graph I will run the algorithms */
+  override def validate(sc: SparkContext, config: Config): SparkJobValidation = {
+    Try(config.getString("schema.string"))
+      .map(x => SparkJobValid)
+      .getOrElse(SparkJobInvalid("No schema.string config param"))
+
+    Try(config.getString("vertices.string"))
+      .map(x => SparkJobValid)
+      .getOrElse(SparkJobInvalid("No vertices.string config param"))
+
+    Try(config.getString("edges.string"))
+      .map(x => SparkJobValid)
+      .getOrElse(SparkJobInvalid("No edges.string config param"))
+
+    Try(config.getString("vertexId.string"))
+      .map(x => SparkJobValid)
+      .getOrElse(SparkJobInvalid("No vertexId.string config param"))
+
+    Try(config.getString("source.string"))
+      .map(x => SparkJobValid)
+      .getOrElse(SparkJobInvalid("No vertices.string config param"))
+
+    Try(config.getString("target.string"))
+      .map(x => SparkJobValid)
+      .getOrElse(SparkJobInvalid("No edges.string config param"))
+
+    Try(config.getString("alpha.string"))
+      .map(x => SparkJobValid)
+      .getOrElse(SparkJobInvalid("No alpha.string config param"))
+
+    Try(config.getString("TOL.string"))
+      .map(x => SparkJobValid)
+      .getOrElse(SparkJobInvalid("No TOL.string config param"))
+  }
+
+  override def runJob(sc: SparkContext, config: Config): Any = {
+
+    /* Received Parameters */
+    val schema = config.getString("schema.string")
+    val strVerticesTable = config.getString("vertices.string")
+    val strEdgesTable = config.getString("edges.string")
+    val strVertexId = config.getString("vertexId.string")
+    val strSource = config.getString("source.string")
+    val strTarget = config.getString("target.string")
+    val alpha = config.getDouble("alpha.string")
+    val TOL = config.getDouble("TOL.string")
+
+    /* Get table from keyspace and stored as rdd */
+    val vertexRDD1: RDD[(VertexId, String)] = sc.cassandraTable(schema, strVerticesTable)
+
+    /* Get Cassandra Row and Select id */
+    val vertexCassandra: RDD[CassandraRow] = sc.cassandraTable(schema, strVerticesTable)
+      .select(strVertexId)
+
+    /* Convert Cassandra Row into Spark's RDD */
+    val rowsCassandra: RDD[CassandraRow] = sc.cassandraTable(schema, strEdgesTable)
+      .select(strSource, strTarget)
+
+    /* Convert RDD into edgeRDD */
+    val edgesRDD: RDD[Edge[Int]] = rowsCassandra.map(x =>
+      Edge(
+        x.getLong(strSource),
+        x.getLong(strTarget)
+      ))
+
+    val vertexSet = VertexRDD(vertexRDD1)
+
+    /* Build the initial Graph */
+    val graph = Graph(vertexSet, edgesRDD)
+
+    /* Run PageRank until convergence*/
+    /* alpha = 0.0001 */
+    val ranks = graph.pageRank(TOL).vertices
+
+    ranks.collect()
+
+  }//runJob
+
+}//TruenoPR object
 
